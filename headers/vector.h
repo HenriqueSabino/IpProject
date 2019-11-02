572d7358c4e9063e816ac4d3cac9473a90b96139
HenriqueSabino <h.sabinocosta@gmail.com>
2019-11-01 21:09:46 -0300

    Adding more vector functions

--- headers/vector.h
+++ headers/vector.h
@@ -8,5 +8,13 @@ typedef struct vector
 } Vector;
 
 Vector create_vector(float x, float y);
+Vector sum(Vector a, Vector b);
+Vector diff(Vector a, Vector b);
+Vector mult(Vector a, float x);
+Vector normalized(Vector a);
+void setMag(Vector *a, float x);
+float getMag(Vector a);
+float dot(Vector a, Vector b);
+float dist(Vector a, Vector b);
 
 #endif // !VECTOR_H
--- sources/vector.c
+++ sources/vector.c
@@ -1,7 +1,48 @@
+#include <math.h>
 #include "../headers/vector.h"
 
 Vector create_vector(float x, float y)
 {
     Vector temp = {.x = x, .y = y};
     return temp;
+}
+
+Vector sum(Vector a, Vector b)
+{
+    return create_vector(a.x + b.x, a.y + b.y);
+}
+
+Vector diff(Vector a, Vector b)
+{
+    return create_vector(a.x - b.x, a.y - b.y);
+}
+
+Vector mult(Vector a, float x)
+{
+    return create_vector(x * a.x, x * a.y);
+}
+
+float getMag(Vector a)
+{
+    return sqrtf(a.x * a.x + a.y * a.y);
+}
+
+Vector normalized(Vector a)
+{
+    return mult(a, 1 / getMag(a));
+}
+
+void setMag(Vector *a, float x)
+{
+    *a = mult(normalized(*a), x);
+}
+
+float dot(Vector a, Vector b)
+{
+    return a.x * b.x + a.y * b.y;
+}
+
+float dist(Vector a, Vector b)
+{
+    return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
 }
