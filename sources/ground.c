050a3bf96c4d4e2f8a8e336f19dd8baa86759a50
hyanbatista42 <54192614+hyanbatista42@users.noreply.github.com>
2019-11-05 00:25:54 -0300

    changes to the ground hitbox

--- sources/ground.c
+++ sources/ground.c
@@ -11,9 +11,20 @@ void init_ground(Ground *ground, Vector pos, int animation_frame)
     ground->rb.velocity = create_vector(0, 0);
     ground-> rb.acceleration = create_vector(0, 0);
     ground->rb.pos = pos;
-    ground-> rb.cb.width = 128;
-    ground-> rb.cb.height = 128;
-    ground-> rb.cb.offset = create_vector(0, 0);
+
+    if (animation_frame == 0 || animation_frame == 2 || animation_frame == 7)
+    {
+        ground-> rb.cb.width = 128;
+        ground-> rb.cb.height = 100;
+        ground-> rb.cb.offset = create_vector(0, 28);
+    }
+    else
+    {
+        ground-> rb.cb.width = 128;
+        ground-> rb.cb.height = 128;
+        ground-> rb.cb.offset = create_vector(0, 0);
+    } 
+    
     ground-> rb.cb.min = create_vector(ground->rb.pos.x, ground->rb.pos.y);
     ground-> rb.cb.max = create_vector(ground->rb.cb.min.x + ground->rb.cb.width, ground->rb.cb.min.y + ground->rb.cb.height);
     strcpy(ground->rb.cb.tag, "ground");
