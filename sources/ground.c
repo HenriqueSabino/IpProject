bc872a60fcfe439458fc4f7a2507839ed0f40892
NelsuLins <nelsonlins2011@hotmail.com>
2019-11-08 01:03:59 -0200

    Minor changes in the ground.c

--- sources/ground.c
+++ sources/ground.c
@@ -12,11 +12,23 @@ void init_ground(Ground *ground, Vector pos, int animation_frame)
     ground-> rb.acceleration = create_vector(0, 0);
     ground->rb.pos = pos;
 
-    if (animation_frame == 0 || animation_frame == 2 || animation_frame == 7)
+    if (animation_frame == 0 || animation_frame == 1 || animation_frame == 2 || animation_frame == 7)
     {
         ground-> rb.cb.width = 128;
         ground-> rb.cb.height = 100;
         ground-> rb.cb.offset = create_vector(0, 28);
+    }
+    else if (animation_frame == 3)
+    {
+        ground-> rb.cb.width = 116;
+        ground-> rb.cb.height = 128;
+        ground-> rb.cb.offset = create_vector(0, 0);
+    }
+    else if(animation_frame == 4)
+    {
+        ground-> rb.cb.width = 116;
+        ground-> rb.cb.height = 128;
+        ground-> rb.cb.offset = create_vector(12, 0);
     }
     else
     {
